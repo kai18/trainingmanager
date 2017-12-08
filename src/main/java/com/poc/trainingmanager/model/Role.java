27d753700b5195e7d2692730b41eaaa4576d7d9a
Kaustubh <kaustubh0163@gmail.com>
2017-12-08 11:46:08 +0530

    Added role POJO for role table

---
+++ src/main/java/com/poc/trainingmanager/model/Role.java
@@ -0,0 +1,119 @@
+package com.poc.trainingmanager.model;
+
+import java.util.Date;
+
+import com.datastax.driver.core.utils.UUIDs;
+
+public class Role {
+
+	private UUIDs roleId;
+	private String roleName;
+	private String roleType;
+	private String roleDescription;
+	private int creationPrevilege;
+	private int readPrevilege;
+	private int deletionPrevilege;
+	private int updationPrevilege;
+	private Date createdDtm;
+	private Date updatedDtm;
+
+	public Role() {
+		super();
+	}
+
+	public Role(UUIDs roleId, String roleName, String roleType, String roleDescription, int creationPrevilege,
+			int readPrevilege, int deletionPrevilege, int updationPrevilege, Date createdDtm, Date updatedDtm) {
+		super();
+		this.roleId = roleId;
+		this.roleName = roleName;
+		this.roleType = roleType;
+		this.roleDescription = roleDescription;
+		this.creationPrevilege = creationPrevilege;
+		this.readPrevilege = readPrevilege;
+		this.deletionPrevilege = deletionPrevilege;
+		this.updationPrevilege = updationPrevilege;
+		this.createdDtm = createdDtm;
+		this.updatedDtm = updatedDtm;
+	}
+
+	public UUIDs getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(UUIDs roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(String roleType) {
+		this.roleType = roleType;
+	}
+
+	public String getRoleDescription() {
+		return roleDescription;
+	}
+
+	public void setRoleDescription(String roleDescription) {
+		this.roleDescription = roleDescription;
+	}
+
+	public int getCreationPrevilege() {
+		return creationPrevilege;
+	}
+
+	public void setCreationPrevilege(int creationPrevilege) {
+		this.creationPrevilege = creationPrevilege;
+	}
+
+	public int getReadPrevilege() {
+		return readPrevilege;
+	}
+
+	public void setReadPrevilege(int readPrevilege) {
+		this.readPrevilege = readPrevilege;
+	}
+
+	public int getDeletionPrevilege() {
+		return deletionPrevilege;
+	}
+
+	public void setDeletionPrevilege(int deletionPrevilege) {
+		this.deletionPrevilege = deletionPrevilege;
+	}
+
+	public int getUpdationPrevilege() {
+		return updationPrevilege;
+	}
+
+	public void setUpdationPrevilege(int updationPrevilege) {
+		this.updationPrevilege = updationPrevilege;
+	}
+
+	public Date getCreatedDtm() {
+		return createdDtm;
+	}
+
+	public void setCreatedDtm(Date createdDtm) {
+		this.createdDtm = createdDtm;
+	}
+
+	public Date getUpdatedDtm() {
+		return updatedDtm;
+	}
+
+	public void setUpdatedDtm(Date updatedDtm) {
+		this.updatedDtm = updatedDtm;
+	}
+
+}
