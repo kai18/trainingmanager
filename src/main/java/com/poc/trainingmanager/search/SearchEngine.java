e7e92d3d599d29dfccbd21bb94fa68d9a7806878
Kaustubh <kaustubh0163@gmail.com>
2017-12-12 14:40:27 +0530

    Adding search engine and service layer

---
+++ src/main/java/com/poc/trainingmanager/search/SearchEngine.java
@@ -0,0 +1,44 @@
+package com.poc.trainingmanager.search;
+
+import java.util.List;
+
+import com.poc.trainingmanager.model.Department;
+import com.poc.trainingmanager.model.Role;
+import com.poc.trainingmanager.model.User;
+
+public class SearchEngine {
+
+	public List<User> searchByFirstNameAndLastName(String firstName, String lastName) {
+		return null;
+	}
+
+	public List<User> searchByFirstName(String firstName) {
+		return null;
+	}
+
+	public List<User> searchByLastName(String lastName) {
+		return null;
+	}
+
+	public List<User> searhByDepartments(List<Department> departments) {
+		return null;
+	}
+
+	public List<User> searchByRoles(List<Role> roles) {
+		return null;
+	}
+
+	public User searchByEmail(String email) {
+		return null;
+	}
+
+	public List<User> searchByPartialEmail(String email) {
+		return null;
+	}
+
+	public List<User> searchByAllParameters(String email, String firstName, String lastName,
+			List<Department> departments, List<Role> roles) {
+		return null;
+	}
+
+}
